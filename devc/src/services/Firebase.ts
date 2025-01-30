957ccba57a6985d9fb9fb46f5a51aa70990cf576
Himal bhusal <mememimi413@gmail.com>
2025-01-30 10:01:45 +0545

    auth

--- devc/src/services/Firebase.ts
+++ devc/src/services/Firebase.ts
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: `${import.meta.env.VITE_APIKEY}`,
-  authDomain: `${import.meta.env.VITE_AUTHDOMAIN}`,
+  authDomain: `devc-seven.vercel.app`,
   projectId: `${import.meta.env.VITE_PROJECTID}`,
   storageBucket: `${import.meta.env.VITE_STORAGE}`,
   messagingSenderId: `${import.meta.env.VITE_MSGSENDINGID}`,
   appId: `${import.meta.env.VITE_APPID}`
-  
+
 };
 
 // Initialize Firebase
